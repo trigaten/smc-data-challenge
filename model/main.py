45893ef82835534294f21ee79e9ca53e2d8eeb45
Gerson Kroiz <gersonkroiz@gmail.com>
2021-07-19 11:23:36 -0400

    added basic training function to main, slightly adjusted model, code appears to compile. NEED TO: train model and debug accordingly

--- model/main.py
+++ model/main.py
@@ -1,9 +1,91 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
+# to make import from one dir up possible
+import sys
+sys.path.append("..") 
+
+# import the custom Dataset object
+import SMCCarsDataset
 
 
+from torch.utils.data import DataLoader
+
+
+
+#from model.py
 from model import UNet
 
+# from torchsummary import summary
+import torch
+import torch.nn as nn
+# from pytorch_unet import UNet
+
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import copy
+import time
+from collections import defaultdict
+# from trainer import Trainer
+from tqdm import tqdm
+from torchsummary import summary
+
+#basic training function
+def training(numEpochs, dataLoader, model, batchSize):
+    print('in training')
+    for epoch in range(numEpochs):
+        for (batch_idx, batch) in enumerate(dataLoader):
+            #define input and labels
+            inputs = batch['image']
+            labels = batch['segmentation']
+            if batch_idx == 0 and epoch == 0:
+                print(summary(model, input_size=(3, 1280, 720)))
+            #output from model
+            out = model(inputs)
+            #calculate loss
+            loss = loss_func(out, labels)
+            # Backward
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+        print(f"\nEpoch: {epoch}/{numEpochs}, Loss: {loss}")
+    torch.save(model.state_dict(), model)
+    print("saved model")
+
+
 if __name__ == "__main__":
+
     #load data
-    #train (write custom function)
+    rootdir = '../SMC21_GM_AV'
+    # SMCCars = SMCCarsDataset.SMCCarsDataset(rootdir)
+
+    #check for GPUs or CPUs
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = UNet(6).to(device)
+
+    #optimizer
+    opt = torch.optim.Adam(model.parameters())
+
+    #binary cross entropy loss function
+    loss_func = nn.BCELoss()
+
+    #load data
+    SMCCars = SMCCarsDataset.SMCCarsDataset(rootdir)
+
+    print(SMCCars.__len__())
+
+    train_data, test_data = torch.utils.data.random_split(SMCCars, [int(np.ceil(SMCCars.__len__()*0.8)), int(np.floor(SMCCars.__len__()*0.2))], generator=torch.Generator())
+    trainDataLoader = DataLoader(train_data, batch_size = 32, shuffle = True)
+    testDataLoader = DataLoader(test_data, batch_size = 32, shuffle = True)
+
+    #number of epochs
+    numEpochs = 10
+    batchSize = 16
+
+    #call training function
+
+    regularLoader = DataLoader(SMCCars, batch_size=batchSize, shuffle=True)
+    training(numEpochs, trainDataLoader, model, batchSize)
+
--- model/model.py
+++ model/model.py
@@ -18,67 +18,67 @@ from torchvision import datasets, transforms
 
 
 #conv2d -> ReLU -> Batch Normalization -> conv2d -> ReLU -> Batch Normalization -> MaxPool2D
-def downBlock(in_channels, out_channels,kernel_size, stride, padding, normalization = True, dropout = 0.1):
+def downBlock(in_channels, out_channels,kernel_size, stride = 1, padding = 0, normalization = True, dropout = 0.1):
 
-    # # 4 in channels: R,G,B,alpha
-    # in_channels = 4
-    # out_channels = 16
 
-    # #3x3 or 5x5 is common for kernel size, idk y
-    # kernel_size = 3
-
-    # #padding
-    # padding = 0
 
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size, padding),
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
         nn.ReLU(),
-        nn.BatchNorm2d(out_channels),
-        nn.Conv2d(out_channels, out_channels, kernel_size, padding),
+        # nn.BatchNorm2d(out_channels),
+        nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
         nn.ReLU(),
-        nn.BatchNorm2d(out_channels),
+        # nn.BatchNorm2d(out_channels),
         nn.MaxPool2d(kernel_size, stride, padding)
     )
 
 #convTranspose2d -> ReLU -> BatchNorm2d -> Concatenate -> conv2d -> ReLU -> BatchNorm2d -> conv2d -> ReLU -> BatchNorm2d
-def upBlock(in_channels, out_channels, normalization = True, dropout = 0.1):
+def upBlock(in_channels, out_channels, kernel_size, stride = 1, padding = 0, normalization = True, dropout = 0.1):
     block = nn.Sequential(
-        nn.ConvTranspose2d(in_channels, out_channels),
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
         nn.ReLU(),
-        nn.BatchNorm2d(out_channels),
-        nn.Upsample(),
-        nn.Conv2d(out_channels, out_channels),
+        # nn.BatchNorm2d(out_channels),
+        nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+        nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
         nn.ReLU(),
-        nn>BatchNorm2d(out_channels),
-        nn.Conv2d(out_channels, out_channels),
+        # nn.BatchNorm2d(out_channels),
+        nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
         nn.ReLU(),
-        nn>BatchNorm2d(out_channels),
+        # nn.BatchNorm2d(out_channels),
     )
     return block
 
 #worth implementing dropout, and normalization option
-class CnnUNet(nn.Module):
+class UNet(nn.Module):
 
-    def __init__(self, in_channels, out_channels, n_blocks, start_filter_num):
+    def __init__(self, in_channels = 3, out_channels = 64, n_blocks = 4, start_filter_num = 64):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.n_blocks = n_blocks
         self.start_filter_num = start_filter_num
 
-        self.block1 = downBlock(3, 64)
-        self.block2 = downBlock(64, 128)
+        self.dBlock1 = downBlock(3, 64, 2)
+        self.dBlock2 = downBlock(64, 128, 2)
+        self.dBlock3 = downBlock(128, 256, 2)
+        self.dBlock4 = downBlock(256, 512, 2)
+
         
         #need to check dimensions of stuff
-        self.block3 = upBlock()
-        self.block4 = upBlock()
+        self.uBlock1 = upBlock(512, 256, 2)
+        self.uBlock2 = upBlock(256, 128, 2)
+        self.uBlock3 = upBlock(128, 64, 2)
+        # self.block8 = upBlock(64, )
 
-        self.conv_last = nn.Conv2d(64, 3, 1)
-    def build_model(self, weights):
-        weights = self.block1
-        weights = self.block2
-        weights = self.block3
-        weights = self.block4
+        self.conv_last = nn.Conv2d(64, 2, 1)
+    def forward(self, weights):
+        weights = self.dBlock1(weights)
+        weights = self.dBlock2(weights)
+        weights = self.dBlock3(weights)
+        weights = self.dBlock4(weights)
+        weights = self.uBlock1(weights)
+        weights = self.uBlock2(weights)
+        weights = self.uBlock3(weights)
         out = self.conv_last(weights)
         return out
         #downBlock
@@ -86,93 +86,3 @@ class CnnUNet(nn.Module):
         #upBlock
         #upBlock
         #con2vD
-
-
-####NOT MY CODE
-
-from collections import defaultdict
-import torch.nn.functional as F
-from loss import dice_loss
-
-# def calc_loss(pred, target, metrics, bce_weight=0.5):
-#     bce = F.binary_cross_entropy_with_logits(pred, target)
-        
-#     pred = F.sigmoid(pred)
-#     dice = dice_loss(pred, target)
-    
-#     loss = bce * bce_weight + dice * (1 - bce_weight)
-    
-#     metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
-#     metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
-#     metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
-    
-#     return loss
-
-# def print_metrics(metrics, epoch_samples, phase):    
-#     outputs = []
-#     for k in metrics.keys():
-#         outputs.append("{}: {:4f}".format(k, metrics[k] / epoch_samples))
-        
-#     print("{}: {}".format(phase, ", ".join(outputs)))    
-
-# def train_model(model, optimizer, scheduler, num_epochs=25):
-#     best_model_wts = copy.deepcopy(model.state_dict())
-#     best_loss = 1e10
-
-#     for epoch in range(num_epochs):
-#         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-#         print('-' * 10)
-        
-#         since = time.time()
-
-#         # Each epoch has a training and validation phase
-#         for phase in ['train', 'val']:
-#             if phase == 'train':
-#                 scheduler.step()
-#                 for param_group in optimizer.param_groups:
-#                     print("LR", param_group['lr'])
-                    
-#                 model.train()  # Set model to training mode
-#             else:
-#                 model.eval()   # Set model to evaluate mode
-
-#             metrics = defaultdict(float)
-#             epoch_samples = 0
-            
-#             for inputs, labels in dataloaders[phase]:
-#                 inputs = inputs.to(device)
-#                 labels = labels.to(device)             
-
-#                 # zero the parameter gradients
-#                 optimizer.zero_grad()
-
-#                 # forward
-#                 # track history if only in train
-#                 with torch.set_grad_enabled(phase == 'train'):
-#                     outputs = model(inputs)
-#                     loss = calc_loss(outputs, labels, metrics)
-
-#                     # backward + optimize only if in training phase
-#                     if phase == 'train':
-#                         loss.backward()
-#                         optimizer.step()
-
-#                 # statistics
-#                 epoch_samples += inputs.size(0)
-
-#             print_metrics(metrics, epoch_samples, phase)
-#             epoch_loss = metrics['loss'] / epoch_samples
-
-#             # deep copy the model
-#             if phase == 'val' and epoch_loss < best_loss:
-#                 print("saving best model")
-#                 best_loss = epoch_loss
-#                 best_model_wts = copy.deepcopy(model.state_dict())
-
-#         time_elapsed = time.time() - since
-#         print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-#     print('Best val loss: {:4f}'.format(best_loss))
-
-#     # load best model weights
-#     model.load_state_dict(best_model_wts)
-#     return model
