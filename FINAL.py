7f512ead33355165c6078a18eddbe18566ecd6c1
trigaten <sanderman5@hotmail.com>
2021-08-06 21:24:35 -0400

    Final script

--- FINAL.py
+++ FINAL.py
@@ -7,16 +7,17 @@ __email__ = "sanderschulhoff@gmail.com"
 import sys
 import torchvision.io as IO
 import model from model.model.CnnUNet
+from torchvision.utils import save_image
 
 model = CnnUNet("USE_PRETRAINED_WEIGHTS_FLAG")
 
 # assume folder name is passed as first argument (so is just after file path)
-dir = sys.argv[1]
+directory = sys.argv[1]
 
-for image_file in os.listdir(dir):
+for index, image_file in enumerate(os.listdir(directory)):
     image = IO.read_image(img_path)
     pred_seg_map = model(torch.unsqueeze(image, 0))
-    
+    save_image(pred_seg_map, directory + "/seg" + str(index))
 
 
 
