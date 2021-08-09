4284ac7c1cb58b2366d6f61963044ad9dce8d772
trigaten <sanderman5@hotmail.com>
2021-08-09 19:20:02 -0400

    v2

--- examples/car_transferv2.py
+++ examples/car_transferv2.py
@@ -24,18 +24,18 @@ new_sample_seg = torch.clone(sample['segmentation'])
 new_sample_seg = rgb_to_onehot(new_sample_seg)
 
 # get a random sample
-random_content_sample = SMCCars[1]#[random.randint(0, len(SMCCars)-1)]
+random_content_sample = SMCCars[20]#[random.randint(0, len(SMCCars)-1)]
 
 random_content_seg = random_content_sample['segmentation']
 random_content_seg = rgb_to_onehot(random_content_seg)
 
 # pick a random channel index to swap
-index = 3 #random.randint(0, new_sample_seg.size()[0]-1)
+index = 9 #random.randint(0, new_sample_seg.size()[0]-1)
 
 # add the channel to sample
-# new_sample_seg[index] = torch.max(new_sample_seg[index], random_content_seg[index]) 
+new_sample_seg[index] = torch.max(new_sample_seg[index], random_content_seg[index]) 
 
-# new_image = torch.where(new_sample_seg[index] > 0, random_content_sample['image'], sample['image'])
+new_image = torch.where(random_content_seg[index] > 0, random_content_sample['image'], sample['image'])
 
 fig = plt.figure(figsize=(8, 8))
 # exit()
@@ -55,13 +55,13 @@ ax = fig.add_subplot(3, 2, 4)
 imgplot = plt.imshow(random_content_sample['segmentation'].permute(1, 2, 0))
 ax.set_title('Second seg')
 
-# ax = fig.add_subplot(3, 2, 5)
-# imgplot = plt.imshow(new_image.permute(1, 2, 0))
-# ax.set_title('New Image')
+ax = fig.add_subplot(3, 2, 5)
+imgplot = plt.imshow(new_image.permute(1, 2, 0))
+ax.set_title('New Image')
 
-# ax = fig.add_subplot(3, 2, 6)
+ax = fig.add_subplot(3, 2, 6)
 
-# imgplot = plt.imshow(onehot_to_rgb(new_sample_seg).permute(1, 2, 0))
-# ax.set_title('New Seg')
+imgplot = plt.imshow(onehot_to_rgb(new_sample_seg).permute(1, 2, 0))
+ax.set_title('New Seg')
 
 plt.show()
